cad840fffae27151ca3eae06d058677c58e1021c
HongSic <gpo04174@naver.com>
2022-02-12 20:05:41 +0900

    2022-02-12 Add DBExtension.cs

---
+++ Utils/DBExtension.cs
@@ -0,0 +1,17 @@
+﻿using HS.DB.Command;
+using HS.DB.Param;
+
+namespace HS.DB.Utils
+{
+    public static class DBExtension
+    {
+        public static DBCommand Add(this DBCommand Command, string Name, object Value)
+        {
+            if (Command is DBCommandMySQL) Command.Add(new DBParamMySQL(Name, Value));
+            else if (Command is DBCommandMSSQL) Command.Add(new DBParamMSSQL(Name, Value));
+            else if (Command is DBCommandOracle) Command.Add(new DBParamOracle(Name, Value));
+
+            return Command;
+        }
+    }
+}
