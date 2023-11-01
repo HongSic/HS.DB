588da4344aa0898f1828d9e236d0425b842f4a05
HongSic <gpo04174@hskernel.com>
2023-11-01 16:04:57 +0900

    Add DBConnectionKin.Other

--- DBConnectionKind.cs
+++ DBConnectionKind.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace HS.DB
+﻿namespace HS.DB
 {
     public enum DBConnectionKind
     {
         MSSQL,
         Oracle,
         MySQL,
-        SQLite
+        SQLite,
+        Other
     }
 }
